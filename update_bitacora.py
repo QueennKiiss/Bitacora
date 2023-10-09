43ddc8358ee5e5f6591a06e810ec4c65c380130f
mmaestro <mmaestro@circontrol.com>
2023-10-09 16:34:10 +0200

    refactor: remove print messages

--- update_bitacora.py
+++ update_bitacora.py
@@ -11,7 +11,6 @@ import update_bitacora_manager
 def main() -> None:
     """ Main function to be executed when the .py file will be executed directly"""
     update_bitacora_manager.main('Last week')
-    print("=============== Bitacora Updated ================")
 
 
 if __name__ == "__main__":
